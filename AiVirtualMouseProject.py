2c891b58b3b5dbbdbbdd6c6ba9000fd0698bd1b1
Mehul Uttam <mehulsavio@gmail.com>
2024-11-18 10:42:59 +0530

    replaced autopy

--- AiVirtualMouseProject.py
+++ AiVirtualMouseProject.py
@@ -2,23 +2,23 @@ import cv2
 import numpy as np
 import HandTrackingModule as htm
 import time
-import autopy
+import pyautogui
 
 ##########################
 wCam, hCam = 640, 480
 frameR = 100  # Frame Reduction
-smoothening = 7
+smoothening = 5  # Increase smoothening for smoother movement
 #########################
 
 pTime = 0
-plocX, plocY = 0, 0
-clocX, clocY = 0, 0
+plocX, plocY = 0, 0  # Previous location of the cursor
+clocX, clocY = 0, 0  # Current location of the cursor
 
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 detector = htm.handDetector(maxHands=1)
-wScr, hScr = autopy.screen.size()
+wScr, hScr = pyautogui.size()
 
 while True:
     # 1. Find hand Landmarks
@@ -28,8 +28,8 @@ while True:
 
     # 2. Get the tip of the index and middle fingers
     if len(lmList) != 0:
-        x1, y1 = lmList[8][1:]
-        x2, y2 = lmList[12][1:]
+        x1, y1 = lmList[8][1:]  # Index finger tip
+        x2, y2 = lmList[12][1:]  # Middle finger tip
 
         # 3. Check which fingers are up
         fingers = detector.fingersUp()
@@ -41,12 +41,13 @@ while True:
             # 5. Convert Coordinates
             x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
             y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
+
             # 6. Smoothen Values
             clocX = plocX + (x3 - plocX) / smoothening
             clocY = plocY + (y3 - plocY) / smoothening
 
             # 7. Move Mouse
-            autopy.mouse.move(wScr - clocX, clocY)
+            pyautogui.moveTo(wScr - clocX, clocY, duration=0.01)  # Add a short duration for smoother movement
             cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
             plocX, plocY = clocX, clocY
 
@@ -59,7 +60,7 @@ while True:
             if length < 40:
                 cv2.circle(img, (lineInfo[4], lineInfo[5]),
                            15, (0, 255, 0), cv2.FILLED)
-                autopy.mouse.click()
+                pyautogui.click()
 
     # 11. Frame Rate
     cTime = time.time()
